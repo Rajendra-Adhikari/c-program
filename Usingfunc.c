ec7c39e4264a8a42ee070e9958d352f5000784ac
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-01-15 14:20:05 +0545

    Rajendra:programfunction

---
+++ Usingfunc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void namaste();
+void bonjour();
+int main() {
+    printf("enter f for french and n for nepalese");
+    char ch;
+    scanf("%c",&ch);
+    if (ch =='n'){
+        namaste();
+    } else {
+        bonjour();
+    }
+    return 0;
+}
+void namaste(){
+    printf("Namaste\n");
+}
+void bonjour(){
+    printf("Bonjour");
+}
--- problem.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("problem\n");
-    return 0;
-}
