9e0805d74a5544faa9607f7a5e49866dd6d08a39
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-01-27 21:33:09 +0545

    rajendra:program

---
+++ %c-string.c
@@ -0,0 +1,16 @@
+//take a string input from the user using %c
+#include<stdio.h>
+int main(){
+    char str[100];
+    char ch ;
+    int i=0;
+    while(ch !=  '\n'){
+        scanf("%c",&ch); //taking input from user
+        str[i]=ch ;//putting user value in str
+        i++;
+    }
+    str[i] ='\0';//telling compiler that it is string as due to  %c compiler cannot auto know that
+    puts(str);
+    return 0;
+    
+}
