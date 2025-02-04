c6ae837fef9140e687a0e0fb34d2be1495c91e70
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-02-04 20:10:03 +0545

    rajendra:program-file

--- odd-file.c
+++ odd-file.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 int main(){
     FILE *fptr;
-    fptr = fopen("odd.txt","w");
+    fptr = fopen("odd-file.txt","w");
     int n;
     printf("enter n :");
     scanf("%d",&n);
-    for (int i =1;i<n;i++){
-        if(i %2 != 0){
-            fprintf(fptr,"%d\n",n);
+    for (int i =1;i<=n;i++){
+        if(i % 2 != 0){
+            fprintf(fptr,"%d\n",i);
         }
     }
     fclose(fptr);
