d44da867508db8c46459ef9171816887eef70154
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-02-04 20:05:05 +0545

    rajendra:program

---
+++ odd -file.c
@@ -0,0 +1,17 @@
+//write a program to write all the odd numbers from 1 to n in a file
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr = fopen("odd.txt","w");
+    int n;
+    printf("enter n :");
+    scanf("%d",&n);
+    for (int i =1;i<n;i++){
+        if(i %2 != 0){
+            fprintf(fptr,"%d\n",n);
+        }
+    }
+    fclose(fptr);
+    return 0;
+}
+
