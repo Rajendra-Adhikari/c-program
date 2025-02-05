e278e63d502f3d143e7163eacb84640faa6852d1
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-02-05 20:19:47 +0545

    rajendra:program

---
+++ dynamic-alloc.c
@@ -0,0 +1,18 @@
+//write a program to allocate memory of size m,where n is enter by the user
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int *ptr;
+    int n;
+    printf("enter n :");
+    scanf("%d",&n);
+
+    ptr =(int *)calloc(n,sizeof(int));
+
+    for (int i =0; i<n;i++){
+        printf("%d\n",ptr[i]);
+    }
+    return 0;
+}
+
