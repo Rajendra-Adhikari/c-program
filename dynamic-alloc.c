984f51dc400349e5c6edfbb674390a086c3221e8
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-02-05 20:27:34 +0545

    rajendra:program

--- dynamic-alloc.c
+++ dynamic-alloc.c
@@ -13,6 +13,15 @@ int main(){
     for (int i =0; i<n;i++){
         printf("%d\n",ptr[i]);
     }
+
+    free(ptr); //using free()function
+
+    ptr =(int *)calloc(2,sizeof(int)); //new  allocate
+    
+    for (int i =0; i<2;i++){
+        printf("%d\n",ptr[i]);
+    }
+
     return 0;
 }
 
